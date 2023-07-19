1fe885a0938d5a35a297ac7183d3f0a96f7b3b43
Khalid Oukssim <oukssim2015@gmail.com>
2023-07-19 12:27:22 +0000

    add messages and generated files

---
+++ codegen.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install necessary Go libraries
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+
+# Run the Makefile
+make proto
